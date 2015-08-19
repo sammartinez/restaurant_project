30ca5fd4196bba8aaa3d87bd94ebe4fe699a8b32
Sam Martinez & Dana Sharman <student@epicodus.com>
2015-08-19 15:43:25 -0700

    add test_find and test_deleteAll

--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -86,6 +86,95 @@
                 $result = Restaurant::getAll();
                 $this->assertEquals($test_restaurant, $result[0]);
             }
+
+            function test_getAll()
+            {
+                //Arrange
+                $name = "Drinks";
+                $id = null;
+                $test_Cuisine = new Cuisine($name, $id);
+                $test_Cuisine->save();
+
+                $restaurant = "Aalto";
+                $address = "123 Belmont";
+                $phone = "123-456-7890";
+                $cuisine_id = $test_Cuisine->getId();
+                $test_restaurant = new Restaurant($restaurant, $address, $phone, $cuisine_id, $id);
+                $test_restaurant->save();
+
+                $restaurant2 = "Aalto";
+                $address2 = "123 Belmont";
+                $phone2 = "123-456-7890";
+                $cuisine_id2 = $test_Cuisine->getId();
+                $test_restaurant2 = new Restaurant($restaurant, $address, $phone, $cuisine_id, $id);
+                $test_restaurant2->save();
+
+                //Act
+                $result = Restaurant::getAll();
+
+                //Assert
+                $this->assertEquals([$test_restaurant, $test_restaurant2], $result);
+
+            }
+
+            function test_deleteAll()
+            {
+                //Arrange
+                $name = "Drinks";
+                $id = null;
+                $test_Cuisine = new Cuisine($name, $id);
+                $test_Cuisine->save();
+
+                $restaurant = "Aalto";
+                $address = "123 Belmont";
+                $phone = "123-456-7890";
+                $cuisine_id = $test_Cuisine->getId();
+                $test_restaurant = new Restaurant($restaurant, $address, $phone, $cuisine_id, $id);
+                $test_restaurant->save();
+
+                $restaurant2 = "HobNob";
+                $address2 = "999 somewhere";
+                $phone2 = "234-555-5555";
+                $cuisine_id2 = $test_Cuisine->getId();
+                $test_restaurant2 = new Restaurant($restaurant, $address, $phone, $cuisine_id, $id);
+                $test_restaurant2->save();
+
+                //Act
+                Restaurant::deleteAll();
+
+                //Assert
+                $result = Restaurant::getAll();
+                $this->assertEquals([], $result);
+            }
+
+            function test_find()
+            {
+                //Arrange
+                $name = "Drinks";
+                $id = null;
+                $test_Cuisine = new Cuisine($name, $id);
+                $test_Cuisine->save();
+
+                $restaurant = "Aalto";
+                $address = "123 Belmont";
+                $phone = "123-456-7890";
+                $cuisine_id = $test_Cuisine->getId();
+                $test_restaurant = new Restaurant($restaurant, $address, $phone, $cuisine_id, $id);
+                $test_restaurant->save();
+
+                $restaurant2 = "HobNob";
+                $address2 = "999 somewhere";
+                $phone2 = "234-555-5555";
+                $cuisine_id2 = $test_Cuisine->getId();
+                $test_restaurant2 = new Restaurant($restaurant, $address, $phone, $cuisine_id, $id);
+                $test_restaurant2->save();
+
+                //Act
+                $result = Restaurant::find($test_restaurant->getId());
+
+                //Assert
+                $this->assertEquals($test_restaurant, $result);
+            }
         }
 
  ?>
